fd1f18edd7f9d0686e36a77f2f621a2d4f89219c
AntonioCorreaBosco <antonio.bosco@finvestdtvm.com.br>
2025-11-26 09:46:32 -0300

    Update date list in main function, remove outdated output files, and add run_main.bat script for virtual environment activation

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from datetime import datetime, timedelta
 def main():
     # List of dates to process
     date_list = [
-       '2025-11-18', '2025-11-19',  '2025-11-20', '2025-11-21', '2025-11-24', '2025-11-25'  ]
+        '2025-11-19',   '2025-11-21', '2025-11-24', '2025-11-25'  ]
 
     conn = psycopg2.connect(
         dbname=REDSHIFT_CONFIG["database"],
